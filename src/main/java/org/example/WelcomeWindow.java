ff4cd51c186ecc4999b54f982e77db5e423b2db7
Ruslan Zapolianenko <rusartrade@gmail.com>
2023-07-02 20:40:12 +0300

    WelcomeWindow

--- src/main/java/org/example/WelcomeWindow.java
+++ src/main/java/org/example/WelcomeWindow.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WelcomeWindow extends JFrame {
-   private final JLabel label;
-    private final JButton startButton;
+    private JLabel label;
+    private JButton startButton;
 
     public WelcomeWindow() {
         setTitle("Гра Міста");
@@ -14,6 +16,12 @@ public class WelcomeWindow extends JFrame {
 
         label = new JLabel("Ласкаво просимо до гри Міста!");
         startButton = new JButton("Старт");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose(); // Закрити вітальне вікно
+                new GameWindow(); // Відкрити вікно гри
+            }
+        });
 
         JPanel panel = new JPanel();
         panel.add(label);
