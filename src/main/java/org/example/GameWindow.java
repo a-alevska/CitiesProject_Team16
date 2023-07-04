e323d35f1a1483dc20f0afd77612bbd20def4cf7
Ruslan Zapolianenko <rusartrade@gmail.com>
2023-07-04 21:17:30 +0300

    GameWindow4

--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -32,16 +32,32 @@ public class GameWindow extends JFrame {
         inputField = new JTextField();
         computerLabel = new JLabel();
         computerLabel.setText("Компьютер:");
-        computerLabel.setBorder(new EmptyBorder(20, 10, 300, 20));
         playButton = new JButton("Зробити хід");
 
         JPanel panel = new JPanel();
+        JLabel cityLabel = new JLabel();
+        cityLabel.setText("Введіть назву міста:");
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.add(inputField, BorderLayout.NORTH);
-        panel.add(computerLabel, BorderLayout.CENTER);
-        panel.add(playButton, BorderLayout.SOUTH);
+        panel.setLayout(new GridLayout(1, 2));
+        panel.add(cityLabel);
+        panel.add(inputField);
+        //panel1.add(playButton,BorderLayout.WEST);
+        //panel1.add(computerLabel);
+        panel1.setLayout(new GridLayout(1, 2));
+        panel1.add(playButton);
+        panel1.add(computerLabel);
+        JLabel imageLabel = new JLabel();
+        panel2.add(imageLabel,BorderLayout.CENTER);
+        ImageIcon image=new ImageIcon("logo.png");
+        Image scaledImage = image.getImage().getScaledInstance(500, 310, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        imageLabel.setIcon(scaledIcon);
 
-        add(panel);
+        add(panel, BorderLayout.NORTH);
+        add(panel1,BorderLayout.CENTER);
+        add(panel2,BorderLayout.SOUTH);
 
         setVisible(true);
         inputField.addActionListener(new ActionListener() {
