033326e05c15327d6c6355f9a37fe0d424808224
smagles <lesialist@icloud.com>
2023-07-05 20:02:19 +0200

    GameResultWindow

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -5,5 +5,11 @@ import javax.swing.*;
 public class Game {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(WelcomeWindow::new);
+        try {
+            // Встановлення Nimbus Look and Feel
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
